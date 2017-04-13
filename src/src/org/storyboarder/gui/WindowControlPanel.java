8ed5179e3eafe39e97e178b46d98cb5bc97b5d92
ntp488 <ntp488@gmail.com>
2017-04-13 02:10:16 -0500

    Adds better menubar layout

--- src/src/org/storyboarder/gui/WindowControlPanel.java
+++ src/src/org/storyboarder/gui/WindowControlPanel.java
@@ -7,15 +7,32 @@ import java.awt.*;
  * Created by Nathan on 1/24/2017.
  */
 public class WindowControlPanel extends JMenuBar {
-    private JMenu fileMenu = new JMenu("File");
+    private JMenu fileMenu, exitButton;
     private MainWindow window;
+    private BoxLayout layout;
 
     public WindowControlPanel(MainWindow mainWindow) {
+        fileMenu = new JMenu("File");
+        exitButton = new JMenu();
+        ImageIcon icon = new ImageIcon("images/exitbutton.png");
+        Image newimg = icon.getImage().getScaledInstance(15, 15,  java.awt.Image.SCALE_SMOOTH ) ;
+        icon = new ImageIcon(newimg);
+        exitButton.setIcon(icon);
         window = mainWindow;
-        this.setPreferredSize(new Dimension(100, 35));
+        layout = new BoxLayout(this, BoxLayout.X_AXIS);
+
         this.setBackground(Color.DARK_GRAY);
         this.setBorder(BorderFactory.createEtchedBorder());
+        this.setLayout(layout);
+
+        fileMenu.setForeground(Color.white);
         this.add(fileMenu);
+
+        this.add(Box.createHorizontalGlue());
+
+        exitButton.setForeground(Color.white);
+        exitButton.setBackground(Color.DARK_GRAY);
+        this.add(exitButton);
     }
 
     public MainWindow getMainWindow() {
