d8173487a4054601c5c2747dbc7ec131197b9d12
ntp488 <ntp488@gmail.com>
2017-04-18 00:02:38 -0500

    Main classpath change

--- src/main/storyboarder/StoryBoarderMain.java
+++ src/main/storyboarder/StoryBoarderMain.java
@@ -6,7 +6,7 @@ import javax.swing.*;
 /**
  * Created by Nathan on 1/19/2017.
  */
-public class StoryBoarder {
+public class StoryBoarderMain {
     public static void main(String[] Args) {
         JFrame mainWindow = new MainWindow();
     }
